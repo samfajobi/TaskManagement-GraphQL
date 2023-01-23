6a724accd81f4ebb9a33f306a40a65f19767c9d5
Sam Fajobi <segzyfaj1@gmail.com>
2023-01-23 21:20:21 +0100

    started task server

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
+
+
+
+const app = express();
+
+let URL = "mongodb+srv://Christsam:SamFajobi99@cluster0.otx0rri.mongodb.net/?retryWrites=true&w=majority"
+mongoose.set("strictQuery", false);
+mongoose.connect(URL);
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function () {
+    console.log("Database Connection Successful!!!");
+});
+
+app.use("/graphql", graphqlHTTP({
+    schema,
+    graphiql: true
+    
+}));
+
+const PORT = 4000
+app.listen(PORT, () => {
+    console.log(`App is Running on PORT ${PORT}`)
+});
